ec22d5b7e1ddff707dd00b646fe0765a213ee9a5
bassem eloraby <BASEM12683@GMAIL.COM>
2022-12-22 13:21:35 +0300

    api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+
+connectDB();
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
@@ -12,5 +16,5 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 app.listen(port, err => {
 	if (err)
 		throw err
-	console.log('Server listening on port', port)
+	console.log('Server listening on port'.yellow, port.green)
 })
